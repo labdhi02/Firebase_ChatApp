8536430b49d09dd8b31ec9353a69e9e49aa2e017
labdhi02 <labdhi200@gmail.com>
2025-04-02 00:23:47 +0530

    updates

--- config/firebase.js
+++ config/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBpmPY2ZGpNqEwDNEZHWD43Zx7KNnM5LzQ",
-    authDomain: "chatapp-b6662.firebaseapp.com",
-    projectId: "chatapp-b6662",
-    storageBucket: "chatapp-b6662.appspot.com", // fixed storage bucket URL
-    messagingSenderId: "811257851419",
-    appId: "1:811257851419:web:2e37323f02966fa68a92be"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "", // fixed storage bucket URL
+    messagingSenderId: "",
+    appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
